3a4a3d4d23d0844b7559e0909431ab6d41a48726
Angelina Su <tsarminas@gmail.com>
2021-02-02 19:50:24 -0500

    we're getting there ugh

--- assets/js/main.js
+++ assets/js/main.js
@@ -117,21 +117,22 @@ $(document).ready(function() {
 
 });
 
-// ========================================================================= //
-//  Porfolio isotope and filter
-// ========================================================================= //
-$(window).on('load', function() {
-  var portfolioIsotope = $('.portfolio-container').isotope({
-    itemSelector: '.portfolio-thumbnail',
-    layoutMode: 'fitRows'
-  });
+  // ========================================================================= //
+  //  Porfolio isotope and filter
+  // ========================================================================= //
+  $(window).on('load', function() {
+    var portfolioIsotope = $('.portfolio-container').isotope({
+      itemSelector: '.portfolio-item',
+      layoutMode: 'fitRows'
+    });
 
-  $('#portfolio-flters li').on('click', function() {
-    $("#portfolio-flters li").removeClass('filter-active');
-    $(this).addClass('filter-active');
+    $('#portfolio-flters li').on('click', function() {
+      $("#portfolio-flters li").removeClass('filter-active');
+      $(this).addClass('filter-active');
 
-    portfolioIsotope.isotope({
-      filter: $(this).data('filter')
+      portfolioIsotope.isotope({
+        filter: $(this).data('filter')
+      });
     });
   });
 
@@ -139,4 +140,11 @@ $(window).on('load', function() {
   $(document).ready(function() {
     $('.venobox').venobox();
   });
-});
+
+  // Portfolio details carousel
+  $(".portfolio-details-carousel").owlCarousel({
+    autoplay: true,
+    dots: true,
+    loop: true,
+    items: 1
+  });
